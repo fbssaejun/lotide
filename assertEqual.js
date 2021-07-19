799a27791ec428ccb4a9af25a05bce5beac3193e
Anthony Kim <fbssaejun@gmail.com>
2021-07-19 21:04:38 +0000

    checks if two arguments matches and prints the result

---
+++ assertEqual.js
@@ -0,0 +1,11 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  actual === expected ? console.log(`✅Assertion Passed: ${actual} === ${expected}`) : console.log(`🛑Assertion Failed: ${actual} !== ${expected}`);
+};
+
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("hi", "hi");
+assertEqual(1,3);
