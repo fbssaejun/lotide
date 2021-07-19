864024cb1fc373bb8bdbdf077f90be07e37b9a55
Anthony Kim <fbssaejun@gmail.com>
2021-07-19 21:17:48 +0000

    returns first element from input array

---
+++ head.js
@@ -0,0 +1,12 @@
+const assertEqual = function(actual, expected) {
+  actual === expected ? console.log(`✅Assertion Passed: ${actual} === ${expected}`) : console.log(`🛑Assertion Failed: ${actual} !== ${expected}`);
+};
+
+const head = function (arr) {
+  return arr[0];
+}
+
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head([]), 1)
