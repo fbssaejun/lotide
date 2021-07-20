effdda5cf3cd50c6eb5f5d8c5d15c03ebc03f25b
Anthony Kim <fbssaejun@gmail.com>
2021-07-20 22:29:30 +0000

    wrote a function to check if two different arrays are identical

---
+++ eqArrays.js
@@ -0,0 +1,17 @@
+const eqArrays = (firstArr, secondArr) => {
+  if(firstArr.length === secondArr.length) {
+  for(let i = 0; i < firstArr.length; i++) {
+    if(firstArr[i] !== secondArr[i]) {
+      return false;
+    } 
+   }
+   return true;
+  }
+}
+
+const assertEqual = function(actual, expected) {
+  eqArrays(actual, expected) ? console.log(`✅Assertion Passed: ${actual} === ${expected}`) : console.log(`🛑Assertion Failed: ${actual} !== ${expected}`);
+};
+
+
+assertEqual([1, 2, 3], [1, 2, 3]); 
