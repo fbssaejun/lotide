3a28e1566dc6642b745daf226d7f8cb37078e013
Anthony Kim <fbssaejun@gmail.com>
2021-07-21 21:14:00 +0000

    corrected type error for the function name

--- assertArrayEqual.js
+++ assertArrayEqual.js
@@ -10,7 +10,7 @@ const eqArrays = (firstArr, secondArr) => {
 }
 
 
-const assertArrayEqual = (actual, expected) => {
+const assertArraysEqual = (actual, expected) => {
   eqArrays(actual, expected) ? console.log(`✅Assertion Passed: ${actual} === ${expected}`) : console.log(`🛑Assertion Failed: ${actual} !== ${expected}`);
 }
 
