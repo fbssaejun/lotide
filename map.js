17254a622b0fed35e57174842cf2cfcc737dd3bd
Anthony Kim <fbssaejun@gmail.com>
2021-07-22 19:28:17 +0000

    created a function that maps through an array and returns a new array with callback function

---
+++ map.js
@@ -0,0 +1,37 @@
+//Assertion function
+const eqArrays = (firstArr, secondArr) => {
+  if(firstArr.length === secondArr.length) {
+  for(let i = 0; i < firstArr.length; i++) {
+    if(firstArr[i] !== secondArr[i]) {
+      return false;
+    } 
+   }
+   return true;
+  }
+}
+
+const assertArraysEqual = (actual, expected) => {
+  eqArrays(actual, expected) ? console.log(`✅Assertion Passed: ${actual} === ${expected}`) : console.log(`🛑Assertion Failed: ${actual} !== ${expected}`);
+}
+
+
+
+const map = function(array, callback) {
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  }
+  return results;
+}
+
+
+//Test Cases : 
+const words = ["ground", "control", "to", "major", "tom"];
+const results1 = map(words, word => word[0]);
+const results2 = map(words, word => word[1]);
+const results3 = map(words, word => word[word.length - 1]);
+
+assertArraysEqual(results1, ['g','c','t','m','t'])
+assertArraysEqual(results2, ['r','o','o','a','o'])
+assertArraysEqual(results3, ['d','l','o','r','m'])
+
