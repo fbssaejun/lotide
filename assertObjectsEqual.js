19f88c837efa2562caaec3dcb72f787a9f602457
Anthony Kim <fbssaejun@gmail.com>
2021-07-22 03:13:53 +0000

    eslint fix

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,41 +1,41 @@
 const eqArrays = (firstArr, secondArr) => {
-  if(firstArr.length === secondArr.length) {
-  for(let i = 0; i < firstArr.length; i++) {
-    if(firstArr[i] !== secondArr[i]) {
-      return false;
-    } 
-   }
-   return true;
-  } 
-}
+  if (firstArr.length === secondArr.length) {
+    for (let i = 0; i < firstArr.length; i++) {
+      if (firstArr[i] !== secondArr[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
+};
 
 const eqObjects = function(object1, object2) {
-  let keys1 = Object.keys(object1)
-  let keys2 = Object.keys(object2)
+  let keys1 = Object.keys(object1);
+  let keys2 = Object.keys(object2);
   let conditionalCheck = true;
-  if(keys1.length !== keys2.length) {
-      conditionalCheck = false;
-    } else {
-      for(const key1 of keys1) {
-        //Checks for values that are an array
-        if(Array.isArray(object1[key1]) && Array.isArray(object2[key1])) {
-          if (!eqArrays(object1[key1], object2[key1])) {
-            conditionalCheck = false;
-          } else {
-            conditionalCheck = true;
-          }    
-        } else {
-        //Checks for primitive values 
-        if(object1[key1] !== object2[key1]) {
+  if (keys1.length !== keys2.length) {
+    conditionalCheck = false;
+  } else {
+    for (const key1 of keys1) {
+      //Checks for values that are an array
+      if (Array.isArray(object1[key1]) && Array.isArray(object2[key1])) {
+        if (!eqArrays(object1[key1], object2[key1])) {
           conditionalCheck = false;
         } else {
-         conditionalCheck = true;
+          conditionalCheck = true;
         }
-        }    
+      } else {
+        //Checks for primitive values
+        if (object1[key1] !== object2[key1]) {
+          conditionalCheck = false;
+        } else {
+          conditionalCheck = true;
+        }
+      }
     }
   }
   return conditionalCheck;
-}
+};
 
 
 const assertObjectsEqual = function(actual, expected) {
@@ -44,7 +44,7 @@ const assertObjectsEqual = function(actual, expected) {
 };
 
 
-assertObjectsEqual({a : 1}, {a : 1})
-assertObjectsEqual({a : 1}, {a : 2})
-assertObjectsEqual({a : 1, b : [ 1 , 2 ]}, {a : 1, b : [ 1 , 2 ]})
-assertObjectsEqual({ a: '1', b: 2 } , { b: 2, a: '1' })
+assertObjectsEqual({a : 1}, {a : 1});
+assertObjectsEqual({a : 1}, {a : 2});
+assertObjectsEqual({a : 1, b : [ 1 , 2 ]}, {a : 1, b : [ 1 , 2 ]});
+assertObjectsEqual({ a: '1', b: 2 } , { b: 2, a: '1' });
