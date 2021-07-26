2edeb8c2d7aa4b3e37360e7781d90b3f098040bc
Anthony Kim <fbssaejun@gmail.com>
2021-07-26 18:59:49 +0000

    exported function and separated assertion cases to another testing file

--- middle.js
+++ middle.js
@@ -1,12 +1,9 @@
-const eqArrays = require('./eqArrays')
-const assertArraysEqual = require('./assertArraysEqual')
-
-
-
 const middle = (array) => {
   const midIndex = Math.floor(array.length/2);
   const middleArray = [];
-  if(array.length <= 2) {
+  if(!array.length) {
+    return undefined
+  } else if(array.length <= 2) {
     return []
   } else if (array.length % 2 !== 0) {
      middleArray.push(array[midIndex])
