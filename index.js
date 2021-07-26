2872e2d93e5b251dc6ffd113b4ba7ee8c3f1f462
Anthony Kim <fbssaejun@gmail.com>
2021-07-26 21:06:12 +0000

    refactored file (shortended with ES6 shortening syntax)

--- index.js
+++ index.js
@@ -15,18 +15,18 @@ const without = require('./without');
 
 
 module.exports = {
-  head:   head,
-  tail:   tail,
-  middle: middle,
-  countedLetters: countedLetters,
-  countOnly: countOnly,
-  eqArrays: eqArrays,
-  eqObjects: eqObjects,
-  findKey: findKey,
-  findKeyByValue: findKeyByValue,
-  flatten: flatten,
-  letterPositions: letterPositions,
-  map: map,
-  takeUntil: takeUntil,
-  without: without
+  head,
+  tail,
+  middle,
+  countedLetters,
+  countOnly,
+  eqArrays,
+  eqObjects,
+  findKey,
+  findKeyByValue,
+  flatten,
+  letterPositions,
+  map,
+  takeUntil,
+  without
 };
