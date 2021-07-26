cba2bf910c9f6f52cdcf6a051f6b73d5cac0ac70
Anthony Kim <fbssaejun@gmail.com>
2021-07-26 18:59:09 +0000

    refactored test file with mocha and chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,7 +1,20 @@
+const assert = require('chai').assert
 const middle = require('../middle');
-const assertArraysEqual = require('../assertArraysEqual')
-//TEST CASE:
-console.log(middle([1])) // => returns []
-console.log(middle([1,2,3])) // => returns [2]
-console.log(middle([2,3,4,5,6,7]))  // => returns [4,5]
-assertArraysEqual(middle([1,2,3,4,5]), [3]) // => returns ✅Assertion Passed: 3 === 3}
+
+describe("#middle", () => {
+  it("should return an empty array when passed in an array with one element", () => {
+    assert.deepEqual(middle([1]), [])
+  });
+
+  it("should return an array with the middle element of the input array when the length of the array is an odd number", () => {
+    assert.deepEqual(middle([1,2,3]), [2])
+  });
+
+  it("should return an array of the two middle elements when the length of the input array is even", () => {
+    assert.deepEqual(middle([1,2,3,4,5,6,7,8]), [4,5])
+  });
+
+  it("should return undefined when passed in an empty array", () => {
+    assert.deepEqual(middle([]), undefined)
+  })
+})
