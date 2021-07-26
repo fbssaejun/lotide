ff408a0378fc6fee15fb27a52aeabd2bb0a3da64
Anthony Kim <fbssaejun@gmail.com>
2021-07-26 04:22:01 +0000

    exported function & moved test cases into assertEqual test file in test directory

--- assertEqual.js
+++ assertEqual.js
@@ -3,9 +3,6 @@ const assertEqual = function(actual, expected) {
   actual === expected ? console.log(`✅Assertion Passed: ${actual} === ${expected}`) : console.log(`🛑Assertion Failed: ${actual} !== ${expected}`);
 };
 
+module.exports = assertEqual;
 
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual("hi", "hi");
-assertEqual(1,3);
+
