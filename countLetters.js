66d081db523094e043df299d61bd53a88239e518
Anthony Kim <fbssaejun@gmail.com>
2021-07-26 20:33:33 +0000

    exported function and refactored it

--- countLetters.js
+++ countLetters.js
@@ -1,26 +1,22 @@
-const assertEqual = function(actual, expected) {
-  actual === expected ? console.log(`✅Assertion Passed: ${actual} === ${expected}`) : console.log(`🛑Assertion Failed: ${actual} !== ${expected}`);
-};
-
+//Counts number of each alphabet in a sentence and returns an object
 const countLetters = (sentence) => {
   //initialize an object to return with final result
-  let countedLetters = {}
+  let countedLetters = {};
   //separate each letter of string
-  let separatedSentence = sentence.split(' ').join('')
+  let separatedSentence = sentence.split(' ').join('');
   //loop through the string
-  for(const letters of separatedSentence) {
-  //add each letter to the result object 
+  for (const letters of separatedSentence) {
+  //add each letter to the result object
   //if there's more than one, increment, if not, just add.
-      if (countedLetters[letters]) {
-        countedLetters[letters] += 1;
-      }  else {
-        countedLetters[letters] = 1
-      }
-  //   //return counted object
+    if (countedLetters[letters]) {
+      countedLetters[letters] += 1;
+    }  else {
+      countedLetters[letters] = 1;
+    }
+    //return counted object
     
   }
-  return countedLetters
-}
+  return countedLetters;
+};
 
-console.log(countLetters("Hello Howdy")) // => { H: 2, e: 1, l: 2, o: 2, w: 1, d: 1, y: 1 }
-assertEqual(countLetters("abc")['a'], 1) // => ✅Assertion Passed: 1 === 1
+module.exports = countLetters;
