f674600a1a726a6960dc3c9636811c231aca32c9
Anthony Kim <fbssaejun@gmail.com>
2021-07-26 04:22:55 +0000

    imported assertEqual function, exported head function, moved test cases to headTest file in test directory

--- head.js
+++ head.js
@@ -1,12 +1,7 @@
-const assertEqual = function(actual, expected) {
-  actual === expected ? console.log(`✅Assertion Passed: ${actual} === ${expected}`) : console.log(`🛑Assertion Failed: ${actual} !== ${expected}`);
-};
+const assertEqual = require('./assertEqual');
 
 const head = function (arr) {
   return arr[0];
 }
 
-
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([]), 1)
+module.exports = head;
