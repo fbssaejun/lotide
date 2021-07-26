2485ffc27b1d0454d9ad323c824499173248489a
Anthony Kim <fbssaejun@gmail.com>
2021-07-26 13:45:00 +0000

    imported eqArrays, exported assertArraysEqual function

---
+++ assertArraysEqual.js
@@ -0,0 +1,9 @@
+const eqArrays = require('./eqArrays');
+
+const assertArraysEqual = (actual, expected) => {
+  eqArrays(actual, expected) ? console.log(`✅Assertion Passed: ${actual} === ${expected}`) : console.log(`🛑Assertion Failed: ${actual} !== ${expected}`);
+}
+
+module.exports = assertArraysEqual
+
+
