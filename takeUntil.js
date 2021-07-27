dbfbcf11695da1db600d05e9196f56cf68df59c0
Anthony Kim <fbssaejun@gmail.com>
2021-07-27 02:54:31 +0000

    exported function

--- takeUntil.js
+++ takeUntil.js
@@ -1,6 +1,6 @@
 //Assertion Function
-const eqArrays = require('./eqArrays');
-const assertArraysEqual = require('./assertArraysEqual')
+// const eqArrays = require('./eqArrays');
+// const assertArraysEqual = require('./assertArraysEqual')
 
 
 //Takeuntil Function
@@ -19,16 +19,16 @@ const takeUntil = (array, callback) => {
 module.exports = takeUntil;
 
 //Test cases :
-const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
-const results1 = takeUntil(data1, x => x < 0);
-console.log(results1);
+// const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+// const results1 = takeUntil(data1, x => x < 0);
+// console.log(results1);
 
-console.log('---');
+// console.log('---');
 
-const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
-const results2 = takeUntil(data2, x => x === ',');
-console.log(results2);
+// const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+// const results2 = takeUntil(data2, x => x === ',');
+// console.log(results2);
 
 
-assertArraysEqual(takeUntil([1,2,3,4], x => x === 4), [1,2,3]);
-assertArraysEqual(takeUntil([1, 2, 5, 7, 2, -1, 2, 4, 5], x => x < 0), [1,2,5,7,2]);
+// assertArraysEqual(takeUntil([1,2,3,4], x => x === 4), [1,2,3]);
+// assertArraysEqual(takeUntil([1, 2, 5, 7, 2, -1, 2, 4, 5], x => x < 0), [1,2,5,7,2]);
